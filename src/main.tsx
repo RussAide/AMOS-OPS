22337ace343a0cd51626565d6a12b0a8efc0c8df
RussAide <aideyanruss@gmail.com>
2026-07-10 04:24:54 +0100

    Latest fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { TRPCProvider } from "./providers/trpc";
 import App from "./App";
+import "./index.css";
 
 const router = createBrowserRouter([
   { path: "*", element: <App /> },
